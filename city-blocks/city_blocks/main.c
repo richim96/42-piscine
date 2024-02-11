3882cfe3710547918eb2401bd3835aa053d7e911
Riccardo Mei <rmei@Riccardos-MacBook-Pro.local>
2024-02-11 12:10:58 +0100

    edit main.c to include changes to other .c files needed to compile

--- city-blocks/city_blocks/main.c
+++ city-blocks/city_blocks/main.c
@@ -10,29 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "guard_unistd.h"
+#include "guard_lib.h"
 
-int	ft_is_not_format(char *str, int g);
+int		ft_is_not_format(char *str, int g);
+char	**ft_matrix_2d_init(int grid_size);
+void	ft_matrix_2d_print(char **matrix_2d, int grid_size);
 
 /* main() ensures input correctness before calling the solving function */
 int	main(int argc, char **argv)
 {
 	char	*matrix_1d;
+	char	**matrix_2d;
+	char	*error_msg;
 	int		grid_size;
 
 	grid_size = 4;
+	error_msg = "Error\n";
 	if (argc != 2)
 	{
-		write(2, "Error\n", 6);
-		return (-1); 
+		write(2, error_msg, sizeof(error_msg) - 1);
+		return (-1);
 	}
 	matrix_1d = argv[1];
 	if (ft_is_not_format(matrix_1d, grid_size))
 	{
-		write(2, "Error\n", 6);
+		write(2, error_msg, sizeof(error_msg) - 1);
 		return (-1);
 	}
-	// ft_matrix_write(matrix_1d);
-	write(1, "Success\n", 8);
+	matrix_2d = ft_matrix_2d_init(grid_size);
+	ft_matrix_2d_print(matrix_2d, grid_size);
 	return (0);
 }
